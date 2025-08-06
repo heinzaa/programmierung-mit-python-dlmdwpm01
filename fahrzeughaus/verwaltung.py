52f00335d39215a4dff5528f0a62483d9f78674d
Marc Heintz <heintz.marc@web.de>
2025-08-06 20:09:51 +0200

    Fahrzeughausverwaltungsklasse

---
+++ fahrzeughaus/verwaltung.py
@@ -0,0 +1,21 @@
+from fahrzeug import Fahrzeug
+
+class FahrzeughausVerwaltung:
+    def __init__(self):
+        self.fahrzeuge = []
+
+    def fahrzeug_hinzufuegen(self, fahrzeug: Fahrzeug):
+        self.fahrzeuge.append(fahrzeug)
+        print(f"{fahrzeug.marke} {fahrzeug.modell} wurde hinzugefügt.")
+
+    def fahrzeug_entfernen(self, fahrzeug: Fahrzeug):
+        if fahrzeug in self.fahrzeuge:
+            self.fahrzeuge.remove(fahrzeug)
+            print(f"{fahrzeug.marke} {fahrzeug.modell} wurde entfernt.")
+        else:
+            print("Fahrzeug nicht gefunden.")
+
+    def zeige_alle_fahrzeuge(self):
+        print("Fahrzeuge im Fahrzeughaus:")
+        for f in self.fahrzeuge:
+            f.zeige_fahrzeug_informationen()
