75db30e42d41ffd5742330dcfcf9ebbab494ab92
Marc Heintz <heintz.marc@web.de>
2025-08-24 18:56:48 +0200

    Prüfung der Ladungsobergrenze

--- fahrzeughaus/lkw.py
+++ fahrzeughaus/lkw.py
@@ -1,9 +1,10 @@
 from fahrzeug import Fahrzeug
 
 class LKW(Fahrzeug):
-    def __init__(self, marke, modell, baujahr, anzahl_reifen, preis, ladegewicht=0):
+    def __init__(self, marke, modell, baujahr, anzahl_reifen, preis, max_ladegewicht, ladegewicht=0):        
         super().__init__(marke, modell, baujahr, anzahl_reifen, preis)
         self.ladegewicht = ladegewicht
+        self.max_ladegewicht = max_ladegewicht
 
     def starten(self):
         print(f"{self.marke} {self.modell} startet seinen Motor.")
@@ -12,5 +13,8 @@ class LKW(Fahrzeug):
         print(f"Beim {self.marke} {self.modell} werden {self.anzahl_reifen} Reifen mit Spezialwerkzeug in einer großen Werkstatt gewechselt.")
 
     def ladung_aufnehmen(self, gewicht):
-        self.ladegewicht += gewicht
-        print(f"{self.marke} {self.modell} hat nun {self.ladegewicht} Kilogram Ladung aufgenommen.")
+        if self.ladegewicht + gewicht > self.max_ladegewicht:
+            print(f"Achtung: Maximales Ladegewicht von {self.max_ladegewicht} kg wird überschritten!")
+        else:
+            self.ladegewicht += gewicht
+            print(f"{self.marke} {self.modell} hat nun {self.ladegewicht} Kilogram Ladung aufgenommen.")
